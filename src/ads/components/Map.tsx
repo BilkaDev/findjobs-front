3919d65069ca4104064d497fa16d2c198fd1ccb6
Marcin Bilka <bilkadev@gmail.com>
2022-05-20 19:37:01 +0200

    Add map components and styles map.

---
+++ src/ads/components/Map.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {AdEntity} from '../../../../findjobs-back/types/ad-entity';
+import '../../utils/fix-map-icon';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import 'leaflet/dist/leaflet.css'
+import './Map.css'
+
+
+interface Props {
+    ads: AdEntity[];
+}
+
+export const Map = (props: Props) => {
+    function clickHandler() {
+        console.log("setAd")
+    }
+
+    return (
+        <div className="Map">
+            <MapContainer center={[52.3862034, 18.6043395]} zoom={7}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                {props.ads.map(ad => (
+                    <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                        <Popup className="Map__popup">
+                            <div onClick={clickHandler}  className="Map__poput--click">
+                                {ad.title}
+                            </div>
+                        </Popup>
+                    </Marker>))}
+            </MapContainer>
+
+        </div>
+    );
+}
--- src/ads/pages/Ads.tsx
+++ src/ads/pages/Ads.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 
-import './Ads.css'
 import {AdsList} from "../components/AdsList";
+import {Map} from '../components/Map'
+import './Ads.css'
 
 export const Ads = () => {
 
     const DUMMY_ADS = [
-            {
-                id: 'adsid1',
-                name: 'Firma',
-                image: 'https://cdn.pixabay.com/photo/2021/08/11/18/06/paladin-6539115_960_720.png',
-                title: 'Junior Javascript, React',
-                address: 'Katowice, mariacka 4',
-                salaryMin: 2000,
-                salaryMax: 3000,
-                technology: 'JS,React',
-                lat: 52.0932937,
-                lon: 14.3430061,
-            },
         {
-            id: 'adsid2',
-            name: 'Firma 1',
+            id: 'adsid1',
+            name: 'Firma',
             image: 'https://cdn.pixabay.com/photo/2021/08/11/18/06/paladin-6539115_960_720.png',
             title: 'Junior Javascript, React',
             address: 'Katowice, mariacka 4',
@@ -30,7 +19,19 @@ export const Ads = () => {
             lat: 52.0932937,
             lon: 14.3430061,
         },
-        ]
+        {
+            id: 'adsid2',
+            name: 'Firma 1',
+            image: 'https://cdn.pixabay.com/photo/2021/08/11/18/06/paladin-6539115_960_720.png',
+            title: 'Junior Javascript, React',
+            address: 'Katowice, mariacka 4',
+            salaryMin: 2000,
+            salaryMax: 3000,
+            technology: 'JS,React',
+            lat: 52.3862034,
+            lon: 18.6043395,
+        },
+    ]
 
 
     return (
@@ -40,9 +41,10 @@ export const Ads = () => {
                     <h2>search</h2>
                     <h2>sort</h2>
                 </div>
-            </div>
-            <AdsList
-                items={DUMMY_ADS}
+                <AdsList
+                    items={DUMMY_ADS}
                 />
+            </div>
+            <Map ads={DUMMY_ADS}/>
         </div>)
 }
