bf80c80659eaff48195c47de4927e066f14efe81
Marcin Bilka <bilkadev@gmail.com>
2022-06-15 22:46:37 +0200

    Added icon to Map and made a possibility change view map center.

--- src/ads/components/Map.tsx
+++ src/ads/components/Map.tsx
@@ -1,44 +1,64 @@
-import React from 'react';
-import {SimpleAdEntity} from 'types';
+import React, {useEffect, useState} from 'react';
+import {SimpleAdEntity, AdEntity} from 'types';
 import '../../common/utils/fix-map-icon';
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
 import {useNavigate} from "react-router-dom";
-import './Map.css'
+import './Map.css';
+import iconBlack from './../../assets/images/marker-black.png';
+import iconBlue from './../../assets/images/marker-blue.png';
+import {icon, LatLngExpression} from "leaflet";
+import {ChangeView} from "./ChangeView";
 
 
 interface Props {
     ads: SimpleAdEntity[];
+    ad: AdEntity | undefined;
     adId: string | undefined;
 }
 
 export const Map = (props: Props) => {
-    const {adId, ads} = props;
-
     const nav = useNavigate();
+    const [centerLocation, setCenterLocation] = useState<LatLngExpression>([52.3862034, 18.6043395]);
+
+    useEffect(() => {
+        if (props.ad) {
+            setCenterLocation([props.ad.lat, props.ad.lon]);
+        }
+    }, [props.ad]);
 
     function clickHandler(adId: string) {
-        nav(`/ad/${adId}`)
+        nav(`/ad/${adId}`);
     }
+
+    const drawMarkers = props.ads.map(ad => (
+        <Marker key={ad.id}
+                position={[ad.lat, ad.lon]}
+                icon={icon(
+                    {
+                        iconUrl: (props.ad !== undefined && props.ad.id === ad.id) ? iconBlack : iconBlue
+                    })
+                }>
+            <Popup className="Map__popup">
+                <div onClick={() => clickHandler(ad.id)} className="Map__poput--click">
+                    {ad.title}
+                </div>
+            </Popup>
+        </Marker>));
 
     return (
         <div className="Map">
             <MapContainer
-                center={props.ads.length === 1 ? [props.ads[0].lat, props.ads[0].lon] : [52.3862034, 18.6043395]}
+                center={centerLocation}
                 zoom={7}>
+                <ChangeView coords={centerLocation}/>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                {props.ads.map(ad => (
-                    <Marker key={ad.id} position={[ad.lat, ad.lon]}>
-                        <Popup className="Map__popup">
-                            <div onClick={() => clickHandler(ad.id)} className="Map__poput--click">
-                                {ad.title}
-                            </div>
-                        </Popup>
-                    </Marker>))}
+                {drawMarkers}
+
             </MapContainer>
 
         </div>
     );
-}
+};
